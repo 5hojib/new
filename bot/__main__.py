0889feefc3de8a92c7755b1718df069e83f645a0
5hojib <yesiamshojib@gmail.com>
2024-06-25 12:47:19 +0600

    update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,46 +1,47 @@
+import re
+from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
-from pyrogram.filters import command
-
 from bot import LOGGER, bot
-
 from bot.modules import in_channel
 from bot.helpers.database import add_user, present_user
 from bot.helpers.pyro import ButtonMaker, sendMessage
 from bot.helpers.encryption import decrypt
 
 help_msg = "Hello"
-start_msg = "hello"
-
-import re
+start_msg = "Hello"
 
 def extract_ids(url):
     pattern = r"https://t\.me/c/(\d+)/(\d+)"
     match = re.match(pattern, url)
-    
-    chat_id, message_id = match.groups()
-    return int(chat_id), int(message_id)
+    if match:
+        chat_id, message_id = match.groups()
+        return int(chat_id), int(message_id)
+    return None, None
 
-
-async def help(_, message):
+async def help_command(_, message):
     await sendMessage(message, help_msg)
 
-async def start(_, message):
-    if message.chat.type.BOT:
+async def start_command(_, message):
+    if message.chat.type == "private":
         user_id = message.from_user.id
         if not await present_user(user_id):
             await add_user(user_id)
+
     if len(message.command) > 1 and len(message.command[1]) > 10:
         link = message.command[1]
         link = decrypt(link)
         cid, mid = extract_ids(link)
-        cid = f"-100{cid}"
-        cid = int(cid)
-        return await bot.copy_message(int(message.chat.id), cid, mid)
+        if cid and mid:
+            cid = int(f"-100{cid}")
+            await bot.copy_message(message.chat.id, cid, mid)
+            return
+
     await sendMessage(message, start_msg)
 
+
 async def main():
-    bot.add_handler(MessageHandler(start, filters=command('start')))
-    bot.add_handler(MessageHandler(help, filters=command('help')))
+    bot.add_handler(MessageHandler(start_command, filters.command('start')))
+    bot.add_handler(MessageHandler(help_command, filters.command('help')))
     LOGGER.info("Bot Started!")
 
 
